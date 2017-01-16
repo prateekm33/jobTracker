c782492bfb4b0fd36a2a7467095ebf96ca23f519
Prateek Madapurmath <prateekm33@gmail.com>
2017-01-15 23:36:31 -0800

    (update) update configs for prod

--- server/server.js
+++ server/server.js
@@ -27,4 +27,5 @@ app.use(passport.session());
 
 require('./routes.js')(app);
 
-app.listen(3000, () => { console.log('Listening on PORT 3000')})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log('Listening on PORT 3000')})
