7ba5af2de2463891901e8b13acdbfae569a4e284
Prateek Madapurmath <prateekm33@gmail.com>
2017-01-15 22:52:33 -0800

    (feat) updates webpack configs

--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -10,27 +10,5 @@ module.exports = webpackMerge(commonConfig, {
     filename: '[name].bundle.js'
   },
 
-
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-          screw_ie8: true
-      },
-      comments: false
-    })
-  ],
-
-
   devtool: 'cheap-module-source-map'
 })
